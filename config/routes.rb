09fd17bf0120f12e52bf7c2563d52842d4675a31
Veronica Novikova <veronica.novikova@gmail.com>
2023-02-22 17:07:23 +0100

    Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :restaurants
+  # Defines the root path route ('/')
+  root 'restaurants#index'
+  resources :restaurants do
+    resources :reviews, only: [:index, :new, :create]
+  end
+  resources :reviews, only: [:show, :edit, :update, :destroy]
+
 end
