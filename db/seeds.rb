fdde0349e62e971abb62344eb4f8459ee3a42db6
Veronica Novikova <veronica.novikova@gmail.com>
2023-02-22 14:39:04 +0100

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+r = Restaurant.new(name: 'Chez Toto', address: '10 rue Monrue', phone_number:'0311111111', category: 'french')
+r.save
+r2 = Restaurant.new(name: 'Xexe', address: '20 rue Douan', phone_number:'0622522222', category: 'chinese')
+r2.save
+r3 = Restaurant.new(name: 'Dario', address: '30 rue Perrin', phone_number:'0633333333', category: 'italian')
+r3.save
+r4 = Restaurant.new(name: 'Saionara', address: '40 rue Sezam', phone_number:'0644444444', category: 'japanese')
+r4.save
+r5 = Restaurant.new(name: 'Brugges', address: '5 rue Perrin', phone_number:'0655555555', category: 'belgian')
+r5.save
